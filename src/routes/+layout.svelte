dad527bc36cfaa37e625144a79601b6008cd8ca1
Emil Skoglund~ <06skem@skola.engelholm.se>
2024-09-10 19:59:35 +0200

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Display from "../lib/components/Display.svelte";
+    import KeyBoard from "../lib/components/KeyBoard.svelte";
+    import '$lib/global.scss';
+</script>
+
+<Display></Display>
+<slot></slot>
+<KeyBoard></KeyBoard>
